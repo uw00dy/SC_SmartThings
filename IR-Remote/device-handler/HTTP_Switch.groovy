578f2dd2bd790cde8acfc9f14b7028dd57a170fb
Soon Chye <chancsc@gmail.com>
2016-08-06 13:08:22 +0800

    Update HTTP_Switch.groovy

--- IR-Remote/device-handler/HTTP_Switch.groovy
+++ IR-Remote/device-handler/HTTP_Switch.groovy
@@ -7,30 +7,28 @@
 *  Credit: tguerena and surge919
 */
 
-
-preferences {
-	section("External Access"){
-		input "external_on_uri", "text", title: "External On URL", required: false
-		input "external_off_uri", "text", title: "External Off URL", required: false
-	}
-    
-	section("Internal Access"){
-		input "internal_ip", "text", title: "Internal IP", required: false
-		input "internal_port", "text", title: "Internal Port (if not 80)", required: false
-		input "internal_on_path", "text", title: "Internal On Path (/ir?)", required: false
-		input "internal_off_path", "text", title: "Internal Off Path (/ir?)", required: false
-	}
-}
-
-
-
+import groovy.json.JsonSlurper
 
 metadata {
-	definition (name: "HTTP Switch", namespace: "smartthings", author: "Soon Chye") {
-		capability "Actuator"
-			capability "Switch"
-			capability "Sensor"
+	definition (name: "HTTP Switch", namespace: "sc", author: "SC") {
+        capability "Switch"
+		attribute "triggerswitch", "string"
+		command "DeviceTrigger"
 	}
+
+	preferences {
+		input("DeviceIP", "string", title:"Device IP Address", description: "Please enter your device's IP Address", required: true, displayDuringSetup: true)
+		input("DevicePort", "string", title:"Device Port", description: "Empty assumes port 80.", required: false, displayDuringSetup: true)
+		input("DevicePathOn", "string", title:"URL Path for ON", description: "Rest of the URL, include forward slash.", displayDuringSetup: true)
+		input("DevicePathOff", "string", title:"URL Path for OFF", description: "Rest of the URL, include forward slash.", displayDuringSetup: true)
+		input(name: "DevicePostGet", type: "enum", title: "POST or GET", options: ["POST","GET"], defaultValue: "POST", required: false, displayDuringSetup: true)
+		section() {
+			input("HTTPAuth", "bool", title:"Requires User Auth?", description: "Choose if the HTTP requires basic authentication", defaultValue: false, required: true, displayDuringSetup: true)
+			input("HTTPUser", "string", title:"HTTP User", description: "Enter your basic username", required: false, displayDuringSetup: true)
+			input("HTTPPassword", "string", title:"HTTP Password", description: "Enter your basic password", required: false, displayDuringSetup: true)
+		}
+	}
+
 
 	// simulator metadata
 	simulator {
@@ -38,18 +36,12 @@ metadata {
 
 	// UI tile definitions
 	tiles {
-		standardTile("button", "device.switch", width: 2, height: 2, canChangeIcon: true) {
-			state "off", label: 'Off', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState: "on"
-				state "on", label: 'On', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#79b821", nextState: "off"
+		standardTile("DeviceTrigger", "device.triggerswitch", width: 2, height: 2, canChangeIcon: true) {
+			state "triggeroff", label: 'Off', action: "on", icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState: "on"
+			state "triggeron", label: 'On', action: "off", icon: "st.switches.switch.on", backgroundColor: "#79b821", nextState: "off"
 		}
-		standardTile("offButton", "device.button", width: 1, height: 1, canChangeIcon: true) {
-			state "default", label: 'Force Off', action: "switch.off", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
-		}
-		standardTile("onButton", "device.switch", width: 1, height: 1, canChangeIcon: true) {
-			state "default", label: 'Force On', action: "switch.on", icon: "st.switches.switch.on", backgroundColor: "#79b821"
-		}
-		main "button"
-			details (["button","onButton","offButton"])
+		main "DeviceTrigger"
+			details (["DeviceTrigger"])
 	}
 }
 
@@ -58,71 +50,73 @@ def parse(String description) {
 }
 
 def on() {
-	if (external_on_uri){
-		// sendEvent(name: "switch", value: "on")
-		// log.debug "Executing ON"
-
-		def cmd = "${settings.external_on_uri}";
-
-		log.debug "Sending request cmd[${cmd}]"
-
-			httpGet(cmd) {resp ->
-				if (resp.data) {
-					log.info "${resp.data}"
-				} 
-			}
-	}
-	if (internal_on_path){
-		def port
-			if (internal_port){
-				port = "${internal_port}"
-			} else {
-				port = 80
-			}
-
-		def result = new physicalgraph.device.HubAction(
-				method: "GET",
-				path: "${internal_on_path}",
-				headers: [
-				HOST: "${internal_ip}:${port}"
-				]
-				)
-			sendHubCommand(result)
-			sendEvent(name: "switch", value: "on") 
-			log.debug "Executing ON" 
-			log.debug result
-	}
+	log.debug "---ON COMMAND--- ${DevicePathOn}"
+    sendEvent(name: "triggerswitch", value: "triggeron", isStateChange: true)
+	runCmd(DevicePathOn)
 }
 
 def off() {
-	if (external_off_uri){
-		def cmd = "${settings.external_off_uri}";
-		log.debug "Sending request cmd[${cmd}]"
-			httpGet(cmd) {resp ->
-				if (resp.data) {
-					log.info "${resp.data}"
-				} 
-			}
-	}
-	if (internal_off_path){
-		def port
-			if (internal_port){
-				port = "${internal_port}"
-			} else {
-				port = 80
-			}
+	log.debug "---ON COMMAND--- ${DevicePathOff}"
+    sendEvent(name: "triggerswitch", value: "triggeroff", isStateChange: true)
+    runCmd(DevicePathOff)
+}
 
-		def result = new physicalgraph.device.HubAction(
-				method: "GET",
-				path: "${internal_off_path}",
-				headers: [
-				HOST: "${internal_ip}:${port}"
-				]
-				)
+def runCmd(String varCommand) {
+	def host = DeviceIP
+	def LocalDevicePort = ''
+	if (DevicePort==null) { LocalDevicePort = "80" } else { LocalDevicePort = DevicePort }
 
-			sendHubCommand(result)
-			sendEvent(name: "switch", value: "off")
-			log.debug "Executing OFF" 
-			log.debug result
+	def userpassascii = "${HTTPUser}:${HTTPPassword}"
+	def userpass = "Basic " + userpassascii.encodeAsBase64().toString()
+
+	log.debug "The device id configured is: $device.deviceNetworkId"
+
+	def path = varCommand
+	log.debug "path is: $path"
+	//log.debug "Uses which method: $DevicePostGet"
+	def body = "" 
+	//log.debug "body is: $body"
+
+	def headers = [:] 
+	headers.put("HOST", "$host:$LocalDevicePort")
+	headers.put("Content-Type", "application/x-www-form-urlencoded")
+	if (HTTPAuth) {
+		headers.put("Authorization", userpass)
 	}
+	log.debug "The Header is $headers"
+	def method = "POST"
+	try {
+		if (DevicePostGet.toUpperCase() == "GET") {
+			method = "GET"
+			}
+		}
+	catch (Exception e) {
+		settings.DevicePostGet = "POST"
+		log.debug e
+		log.debug "You must not have set the preference for the DevicePOSTGET option"
+	}
+	log.debug "The method is $method"
+	try {
+		def hubAction = new physicalgraph.device.HubAction(
+			method: method,
+			path: path,
+			body: body,
+			headers: headers
+			)
+		log.debug hubAction
+		return hubAction
+	}
+	catch (Exception e) {
+		log.debug "Hit Exception $e on $hubAction"
+	}
+    
+    //sendEvent
+    if (varCommand == "off"){
+    	sendEvent(name: "switch", value: "off")
+        log.debug "Executing OFF"
+    } else {
+    	sendEvent(name: "switch", value: "on")
+        log.debug "Executing ON"
+    }
+    
 }
